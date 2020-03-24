1935741f9848df4bc5cbad112c91e08d5487eb6a
LangJin <fenyukuang@gmail.com>
2020-03-24 20:25:34 +0800

    22222

--- demo01.py
+++ demo01.py
@@ -1,7 +1,51 @@
-print("hello world!")  
-print(2333)
-print(2.333)
-print(True)
-print(())
-print([])
-print({})
+"""
+print("hello world!")  # 字符串
+print(2333)  # 整数
+print(2.333)  # 小数
+print(True)  # 布尔值 True False
+print(())  # 元组
+print([])  # 数组
+print({})  # 字典
+
+锄禾日当午
+汗滴禾下土
+
+print("哈哈",2333,2.333)
+print("哈哈"+"嘻嘻")  # 字符串的拼接
+print("哈哈哈"*100)
+print(((1+2)*100-9.9)/2)
+print(2<3)
+
+# 变量
+# 赋值
+name = "张三"  # 把张三这个值赋值给了名字叫a的这个变量
+print(name)
+
+"""
+
+
+
+# 数据格式的转换
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容：",a+b)
+
+
+# a = "哈哈哈"
+# print(len(a))
+
+"""
+练习：通过代码获取两段内容，并且计算他们的长度的和。
+"""
+a = input("请输入：")
+b = input("请输入：")
+print("两段字符串的长度是：",len(a)+len(b))
