0201e9ca17d6f6f4dfb0130aae01855a22f60c8f
LangJin <fenyukuang@gmail.com>
2020-03-25 22:50:07 +0800

    111

---
+++ demo02.py
@@ -0,0 +1,59 @@
+# 判断  缩进，
+
+# age = int(input("请输入你的年龄："))
+# if age > 60:
+#     print("你应该退休了")
+# elif age > 30:
+#     print("家庭的责任很重吧")
+# elif age > 20:
+#     print("一定要好好规划你的未来！")
+# else:
+#     print("读书的时候，要认真！")
+
+# if age > 20 and age <=30:
+#     print("22222222")
+# elif age > 30:
+#     print("333333333")
+# elif age > 60:
+#     print("66666666")
+# else:
+#     print("xxxxxxxxxx") 
+
+
+# a = input("请输入：")
+# if a == "":
+#     print("不能为空！")
+#     exit()
+# if a in "0123456789":
+#     a = int(a)
+# else:
+#     print("请输入正确的年龄！")
+#     exit()
+# if a > 5:
+#     print("大")
+# else:
+#     print("小")
+
+# a = "dkjfedj"
+# if type(a) is int:
+#     print("是数字！")
+# elif type(a) is str:
+#     print("是字符串")
+# else:
+#     print("其他")
+
+
+# a = 1
+# while a < 10:
+#     print("哈哈哈啊哈",a)
+#     a = a + 2
+
+
+
+# 遍历
+
+# a = "你好，今天你吃了吗？"
+# a = ["张三","李四","王麻子","浪晋","流云","希希","小梁","二狗","陈平安","朱珠","亚索"]
+# for i in a:
+#     print(i)
+
